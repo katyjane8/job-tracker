8c32aed220412248ef6226733f9c2353185c249c
katyjane8 <katy.jane8@gmail.com>
2017-12-10 14:51:38 -0700

    found error to make edit test pass in update method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,19 +21,26 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def edit
-    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job.update(job_params)
+    if @job.save
+      flash[:success] = "#{@job.title} updated!"
+      redirect_to company_job_path(@company, @job)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # implement on your own!
+    @job.destroy
+
+    flash[:success] = "#{@job.title} was successfully deleted!"
+    redirect_to company_jobs_path(@company)
   end
 
   private
