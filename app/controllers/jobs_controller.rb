9bc916edc455c62727cbc83dd1200935a47bbdda
katyjane8 <katy.jane8@gmail.com>
2017-12-10 13:57:35 -0700

    job controller functional with private company method

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,16 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :set_company
 
   def index
-    @company = Company.find(params[:company_id])
     @jobs = @company.jobs
   end
 
   def new
-    @company = Company.find(params[:company_id])
     @job = Job.new()
   end
 
   def create
-    @company = Company.find(params[:company_id])
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
@@ -27,7 +25,7 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @job = Job.find(params[:id])
   end
 
   def update
@@ -44,7 +42,11 @@ class JobsController < ApplicationController
     params.require(:job).permit(:title, :description, :level_of_interest, :city)
   end
 
-  def set_jobs
+  def set_job
     @job = Job.find(params[:id])
+  end
+
+  def set_company
+    @company = Company.find(params[:company_id])
   end
 end
