06e0181c3bef66e473b2cc65f036787950f0f19b
katyjane8 <katy.jane8@gmail.com>
2017-12-10 21:34:23 -0700

    making tests pass with category id

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 describe "User sees one company" do
   scenario "a user sees a company" do
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
+    category = Category.create!(title: "Sportsing")
+
+    company.jobs.create!(title: "Developer", level_of_interest: 90,
+    city: "Denver", category: category)
 
     visit company_path(company)
 
