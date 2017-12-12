de5d6888399178075f47880ad32a61fb33fba85d
katyjane8 <katy.jane8@gmail.com>
2017-12-11 22:33:45 -0700

    need to bug test comments - still not rendering right

---
+++ spec/features/comments/user_can_create_new_comment_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "User creates a new comment" do
+  scenario "a user can create comment" do
+    category = Category.create!(title: "Consulting")
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70,
+                                 city: "Denver", category: category)
+
+    visit company_job_path(company, job)
+
+    fill_in "comment[content]", with: "Submitted Resume"
+
+    click_on "Submit"
+
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
+    expect(page).to have_content("Submitted Resume")
+  end
+end
