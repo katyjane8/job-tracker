739e412c10456f7aedef82c3489b167a237f570b
katyjane8 <katy.jane8@gmail.com>
2017-12-11 22:11:17 -0700

    added controller for comments with create method

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.job_id = params[:job_id]
+    @comment.save
+    redirect_to company_job_path(@comment.job.company_id, @comment.job_id)
+  end
+
+ private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
