af51dbb43dcff88f82befa7efaabfe36f8b737cf
katyjane8 <katy.jane8@gmail.com>
2017-12-11 17:03:52 -0700

    all but two feature tests added

---
+++ spec/features/categories/user_deletes_category_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "User deletes existing category" do
+  scenario "a user can delete a category" do
+    category = Category.create(title: "Dev Ops")
+    visit categories_path
+
+    within(".category_#{category.id}") do
+      click_link "Delete"
+    end
+
+    expect(page).to have_content("ESPN was successfully deleted!")
+  end
+end
