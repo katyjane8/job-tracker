d16764a396b3705e3f7a91713c3ed243399a2efc
katyjane8 <katy.jane8@gmail.com>
2017-12-12 14:38:52 -0700

    just kidding - now tests pass

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -11,6 +11,26 @@ describe Contact do
 
         expect(contact).to be_invalid
       end
+
+      it "is invalid without a position" do
+        contact = Contact.new(name: "Katy", email: "katy@pivitol.com")
+        expect(contact).to be_invalid
+      end
+
+      it "is invalid without an email" do
+        contact = Contact.new(name: "Katy", position: "CTO")
+        expect(contact).to be_invalid
+      end
+
+      context "valid attributes" do
+        it "is valid with a name, position, and email" do
+          company = Company.new(name: "Pivitol Labs")
+          contact = Contact.create!(name: "Katy", position: "CTO",
+          email: "katy@pivitol.com", company: company)
+          expect(contact).to be_valid
+        end
+      end
+
     end
   end
 
