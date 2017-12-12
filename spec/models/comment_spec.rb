abcc4b0eb7fcc8a20a55d604fcc26eeb6b3943f9
katyjane8 <katy.jane8@gmail.com>
2017-12-12 11:12:37 -0700

    add factory bot

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
- describe Comment do
-   describe "validations" do
-     context "invalid attributes" do
-       it "is invalid without content" do
-         comment = Comment.new(job_id: 1)
-         expect(comment).to be_invalid
-       end
-     end
-   end
+describe Comment do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = Comment.new(job_id: 1)
+        expect(comment).to be_invalid
+      end
+    end
+  end
 
-     describe "relationships" do
-       it "belongs to jobs" do
-         comment = Comment.new(content: "hi", job_id: 1)
-         expect(comment).to respond_to(:job)
-       end
-     end
- end
+  describe "relationships" do
+    it "belongs to jobs" do
+      comment = Comment.new(content: "hi", job_id: 1)
+      expect(comment).to respond_to(:job)
+    end
+  end
+end
