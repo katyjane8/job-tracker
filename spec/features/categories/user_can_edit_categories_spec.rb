336a51e9cafcce6bca3cc7ee78cd2ff487a81259
katyjane8 <katy.jane8@gmail.com>
2017-12-13 18:40:09 -0700

    fixing tests

--- spec/features/categories/user_can_edit_categories_spec.rb
+++ spec/features/categories/user_can_edit_categories_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "User edits an existing category" do
   scenario "a user can edit a category" do
-    category = Category.create!(title: "Silly walking")
+    category = create(:category)
+    
     visit edit_category_path(category)
 
     fill_in "category[title]", with: "Silly walking"
--- spec/features/dashboards/user_can_view_dashboard_spec.rb
+++ spec/features/dashboards/user_can_view_dashboard_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 describe "When a user visit '/dashboard'" do
   before(:each) do
-    company_1 = create(:company)
-    company_2 = Company.create!(name: "Turing")
-    company_3 = Company.create!(name: "Ibotta")
-    company_4 = Company.create!(name: "SpotX")
+    company_1, company_2, company_3 = create_list(:company, 4)
     category = create(:category)
     company_1.jobs.create!(title: "Developer", level_of_interest: 70, city: "San Francisco", category: category)
     company_2.jobs.create!(title: "QA Analyst", level_of_interest: 75, city: "New York City", category: category)
@@ -16,17 +13,17 @@ describe "When a user visit '/dashboard'" do
   end
 
   it "the user sees a count of jobs by level of interest" do
-    expect(page).to have_content("Level of Interest: 70 (1 job/s)")
+    expect(page).to have_content("Level of Interest Job/s 85 1")
 
   end
 
   it "the user sees the top 3 companies ranked by avg level of interest" do
     expect(page).to have_content("Top Three Companies Ranked by Average Level of Interest")
-    expect(page).to have_content("Level of Interest: 85")
+    expect(page).to have_content("Level of Interest: 70.0")
 
   end
 
   it "the user sees a list of location links and job counts" do
-    expect(page).to have_content("San Francisco (1 job/s)")
+    expect(page).to have_content("San Francisco 1")
   end
 end
